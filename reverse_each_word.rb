6782f14e3dd76111aff4dcbb50bb6bc0aeeb7c9e
Al Artesona <artesonaa2@gmail.com>
2017-08-02 01:51:07 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(string_sentence)
-  reversed_array =[]
-  string_to_array = string_sentence.split
-  string_to_array.collect do |word|
-    reversed_array << word.reverse
-  end
-  return reversed_array.join(" ")
+  string_sentence.split.collect {|word| word.reverse}.join(" ")
 end
